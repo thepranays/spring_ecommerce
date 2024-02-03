8536c2e0cc59fd94a6c196d7c7ea7cccf0aca4c2
Pranay Payal <58637121+thepranays@users.noreply.github.com>
2024-02-03 14:48:03 +0530

    Initial Eureka Discovery Service

--- inventory_service/src/main/java/com/dreamfist/inventory_service/InventoryServiceApplication.java
+++ inventory_service/src/main/java/com/dreamfist/inventory_service/InventoryServiceApplication.java
@@ -7,7 +7,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
+
 @SpringBootApplication
+@EnableD
 public class InventoryServiceApplication {
 
 	public static void main(String[] args) {
--- product_service/src/main/java/com/dreamfist/product_service/ProductServiceApplication.java
+++ product_service/src/main/java/com/dreamfist/product_service/ProductServiceApplication.java
@@ -1,13 +1,16 @@
 package com.dreamfist.product_service;
 
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+//@EnableEurekaClient
 public class ProductServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProductServiceApplication.class, args);
+		
 	}
 
 }
